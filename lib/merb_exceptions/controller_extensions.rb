9d412c2023212ebc8b8a04e5b3f30e8b88591a3d
Jani Soila <soila@maiko.local>
2008-08-22 20:09:16 +0300

    Updated for Merb 0.9.4 to support current exception passing and changed to use Erubis instead of ERB.

--- lib/merb_exceptions/controller_extensions.rb
+++ lib/merb_exceptions/controller_extensions.rb
@@ -4,9 +4,6 @@ module MerbExceptions
     def self.included(mod)
       mod.class_eval do
         def internal_server_error
-          # These two variables are required by the default exception template
-          @exception = self.params[:exception]
-          @exception_name = @exception.name.split("_").map {|x| x.capitalize}.join(" ")
           self.render_and_notify :layout=>false
         end
       end
@@ -18,12 +15,11 @@ module MerbExceptions
     end
 
     def notify_of_exceptions
-      exception = self.params[:exception]
       request = self.request
-      orig_params = self.params[:original_params]
+
       details = {}
-      details['exception']       = exception
-      details['params']          = orig_params
+      details['exceptions']      = request.exceptions
+      details['params']          = params
       details['environment']     = request.env.merge( 'process' => $$ )
       details['url']             = "#{request.protocol}#{request.env["HTTP_HOST"]}#{request.uri}"
       MerbExceptions::Notification.new(details).deliver!
